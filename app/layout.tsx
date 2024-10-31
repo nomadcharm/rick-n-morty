49265c10598edafe5fd8cf449fd2fbd2938fce9c
Anna Yakubovich <yakubovichannie@gmail.com>
2024-11-01 01:58:08 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.scss";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Rick-n-Morty",
+  description: "A list of `Rick and Morty` characters",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+};
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+import styles from "./scss/page.module.scss";
+
+export default function Home() {
+
+  return (
+    <section className={styles.page}>
+      <h1>Rick and Morty Characters</h1>
+    </section>
+  );
+}
