3563d448b1eba9281ea08062a117fa2b603eaa72
Anna Yakubovich <yakubovichannie@gmail.com>
2024-11-01 18:47:29 +0300

    basic swiper setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 "use client"
+import Image from "next/image";
 import styles from "./scss/page.module.scss";
 import { useGetCharactersQuery } from "./store/Features/CharactersApi/charactersApiSlice";
 import { Character } from "./types/Character";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useRouter } from "next/navigation";
+import { Navigation } from 'swiper/modules'
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Home() {
+  const router = useRouter();
   const { data, isError, isLoading } = useGetCharactersQuery({ page: 1 });
 
   if (isError) {
@@ -19,15 +28,29 @@ export default function Home() {
   return (
     <section className={styles.page}>
       <h1>Rick and Morty Characters</h1>
+      <Swiper
+        className={styles.swiper}
+        spaceBetween={50}
+        slidesPerView={5}
+        navigation={true}
+        scrollbar={{ draggable: true }}
+        loop={true}
+        modules={[Navigation]}
+      >
+        {
+          data?.results.map((character: Character) => {
+            return <SwiperSlide key={character.id} className={styles.slide}>
+              <div className={styles.slideContent}>
+                <h2>{character.name}</h2>
+                <p>Status: {character.status}</p>
+                <Image src={character.image} alt={character.name} width={150} height={150} priority />
+              </div>
+            </SwiperSlide>
+          })
+        }
+      </Swiper>
 
-      <ul>
-        {data?.results.map((character: Character) => (
-          <li key={character.id}>
-            <h2>{character.name}</h2>
-            <p>Status: {character.status}</p>
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => router.push("/characters")}>Смотреть все</button>
     </section>
   );
 }
