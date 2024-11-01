14da553c1a3394c0c930bee9886453276a6b22fb
Anna Yakubovich <yakubovichannie@gmail.com>
2024-11-02 02:36:02 +0300

    basic character page layout and styles

--- app/characters/[character]/page.tsx
+++ app/characters/[character]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useGetCharacterByIdQuery } from "@/app/store/Features/CharactersApi/charactersApiSlice";
 import { use } from "react";
-
+import styles from "../../scss/character.module.scss";
+import Image from "next/image";
 
 interface CharacterParams {
   character: number;
@@ -21,9 +22,26 @@ const Character = ({ params }: { params: Promise<CharacterParams> }) => {
   }
 
   return (
-    <div>
-      <h1>Character: {character?.name}</h1>
-    </div>
+    <section className={styles.character}>
+      <div className={styles.container}>
+        {character && (
+          <div className={styles.wrapper}>
+              <div className={styles.image}>
+                <Image src={character.image} alt={character.name} width={250} height={250} priority />
+              </div>
+              <div className={styles.info}>
+                <h1 className={styles.title}>{character.name}</h1>
+                <p>Status: {character.status}</p>
+                <p>Species: {character.species}</p>
+                {character.type && (<p>Type: {character.type}</p>)}
+                <p>Gender: {character.gender}</p>
+                <p>Planet of Origin: {character.origin.name}</p>
+                <p>Current Location: {character.location.name}</p>
+              </div>
+            </div>
+        )}
+      </div>
+    </section>
   );
 };
 
